079301616035ed1cd9e96480817237be17c695fb
PARAS GUPTA <paras41617@gmail.com>
2023-11-13 11:44:03 +0530

    doc files

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://gist.githubusercontent.com/paras41617/c5be5a5dac800f411cbf6d70fc215846/raw/512c15c81b3bae32e0e8cabd402208eaa82faae6/flask_swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
